c24bd7fbf99d2fc5a417c8061d2c0725bdf19032
vladward <vladward13@gmail.com>
2021-10-02 19:16:08 +0300

    feature: Use Conditional Logic with If Statements

---
+++ tasks/Basic_JavaScript/57.js
@@ -0,0 +1,13 @@
+//Use Conditional Logic with If Statements
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue) {
+        return "Yes, that was true"
+    }
+    return "No, that was false"
+
+
+    // Only change code above this line
+
+}
