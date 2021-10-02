683f956c4b0adbe326ba61225fc6f0996b06f10f
vladward <vladward13@gmail.com>
2021-10-02 19:01:19 +0300

    feature: Assignment with a Returned Value

---
+++ tasks/Basic_JavaScript/54.js
@@ -0,0 +1,12 @@
+//Assignment with a Returned Value
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+processed = processArg(7);
+
+// Only change code below this line
