a7dbc522467ba3b15e2ed564da22ebe8ae2355b6
vladward <vladward13@gmail.com>
2021-10-02 19:13:14 +0300

    feature: Understanding Boolean Values

---
+++ tasks/Basic_JavaScript/56.js
@@ -0,0 +1,10 @@
+//Understanding Boolean Values
+
+function welcomeToBooleans() {
+
+    // Only change code below this line
+
+    return true; // Change this line
+
+    // Only change code above this line
+}
