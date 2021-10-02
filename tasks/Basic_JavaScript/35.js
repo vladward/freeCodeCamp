2c6412f481d3108749817d1b906ad8401d3b2b60
vladward <vladward13@gmail.com>
2021-10-02 18:15:02 +0300

    feature: Use Bracket Notation to Find the Nth-to-Last Character in a String

---
+++ tasks/Basic_JavaScript/35.js
@@ -0,0 +1,7 @@
+//Use Bracket Notation to Find the Nth-to-Last Character in a String
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
