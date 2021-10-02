9c744291866204942a73c98f8dace4b6ff3b54b4
vladward <vladward13@gmail.com>
2021-10-02 19:20:10 +0300

    feature: Comparison with the Inequality Operator

---
+++ tasks/Basic_JavaScript/61.js
@@ -0,0 +1,11 @@
+//Comparison with the Inequality Operator
+
+// Setup
+function testNotEqual(val) {
+    if (val != 99) { // Change this line
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+testNotEqual(10);
