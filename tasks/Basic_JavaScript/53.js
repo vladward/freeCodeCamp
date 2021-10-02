7dc6db873c97e1e97cbb4bd8cc86d263b0fc5d89
vladward <vladward13@gmail.com>
2021-10-02 18:58:42 +0300

    feature: Understanding Undefined Value returned from a Function

---
+++ tasks/Basic_JavaScript/53.js
@@ -0,0 +1,18 @@
+//Understanding Undefined Value returned from a Function
+
+// Setup
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+    sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
