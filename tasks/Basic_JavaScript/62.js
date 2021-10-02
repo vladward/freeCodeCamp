33a63091ec213ab040bd8979f11df8377c238b45
vladward <vladward13@gmail.com>
2021-10-02 19:20:56 +0300

    feature: Comparison with the Strict Inequality Operator

---
+++ tasks/Basic_JavaScript/62.js
@@ -0,0 +1,11 @@
+//Comparison with the Strict Inequality Operator
+
+// Setup
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+testStrictNotEqual(10);
