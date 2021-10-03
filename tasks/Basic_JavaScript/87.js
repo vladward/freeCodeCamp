6dde17dd80eff70a2e089ea4dd83a28202eb72a2
vladward <vladward13@gmail.com>
2021-10-03 10:27:40 +0300

    feature: Delete Properties from a JavaScript Object

---
+++ tasks/Basic_JavaScript/87.js
@@ -0,0 +1,19 @@
+//Delete Properties from a JavaScript Object
+
+// Setup
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+delete myDog.tails;
+
+// Only change code below this line
+
+
+
+
+
+
