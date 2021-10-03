871904d41fd1f3c96871bfad2190b04ea0af918f
vladward <vladward13@gmail.com>
2021-10-03 11:37:00 +0300

    feature: Iterate with JavaScript While Loops

---
+++ tasks/Basic_JavaScript/94.js
@@ -0,0 +1,18 @@
+//Iterate with JavaScript While Loops
+
+// Setup
+var myArray = [];
+var i = 5;
+while(i > -1){
+    myArray.push(i);
+    i--;
+}
+// Only change code below this line
+
+
+
+
+
+
+
+
