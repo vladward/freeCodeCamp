f4ef7b93d7ea9ab81a0f28bf440de3d243f3d5ce
vladward <vladward13@gmail.com>
2021-10-03 11:19:46 +0300

    feature: Manipulating Complex Objects

---
+++ tasks/Basic_JavaScript/90.js
@@ -0,0 +1,33 @@
+//Manipulating Complex Objects
+
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Jilly Boel",
+        "title": "Miano Man",
+        "release_year": 1972,
+        "formats": [
+            "BD",
+            "9T",
+            "SP"
+        ]
+    }
+];
+
+
+
+
+
+
+
+
