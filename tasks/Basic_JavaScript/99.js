41cedaa6734a1f7d31c02ae02bafe9d0e345db99
vladward <vladward13@gmail.com>
2021-10-03 12:12:32 +0300

    feature: Iterate with JavaScript Do...While Loops

--- tasks/Basic_JavaScript/99.js
+++ tasks/Basic_JavaScript/99.js
@@ -1,19 +1,14 @@
-//Nesting For Loops
+//Iterate with JavaScript Do...While Loops
 
-function multiplyAll(arr) {
-    // Only change code below this line
-    var product = 1;
+// Setup
+var myArray = [];
+var i = 10;
 
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr[i].length; j++) {
-            product *= arr[i][j];
-        }
-    }
-
-    return product;
-}
-
-multiplyAll([[1,2],[3,4],[5,6,7]]);
+// Only change code below this line
+do {
+    myArray.push(i);
+    i++;
+} while (i < 5);
 
 
 
