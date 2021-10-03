0d0261653fd07741ce5661a908f2669524dc1fa1
vladward <vladward13@gmail.com>
2021-10-03 11:24:24 +0300

    feature: Accessing Nested Arrays

---
+++ tasks/Basic_JavaScript/92.js
@@ -0,0 +1,30 @@
+//Accessing Nested Arrays
+
+var myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+var secondTree = myPlants[1].list[1];
+
+
+
+
+
+
+
+
