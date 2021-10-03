21a0b46d128c5b53674956e2c36aee2dc2ccff87
vladward <vladward13@gmail.com>
2021-10-03 10:21:05 +0300

    feature: Accessing Object Properties with Variables

---
+++ tasks/Basic_JavaScript/84.js
@@ -0,0 +1,22 @@
+//Accessing Object Properties with Variables
+
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// Only change code below this line
+function numer(number) {
+    var n = 0;
+    return n + number;
+}
+var playerNumber = numer(16); // Change this line
+var player = testObj[playerNumber];   // Change this line
+console.log(player)
+
+
+
+
+
