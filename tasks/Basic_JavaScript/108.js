2fa6818e1e5864d435c4f9df351e7ffd78288e24
vladward <vladward13@gmail.com>
2021-10-03 12:47:17 +0300

    feature: Use Multiple Conditional (Ternary) Operators

---
+++ tasks/Basic_JavaScript/108.js
@@ -0,0 +1,17 @@
+//Use Multiple Conditional (Ternary) Operators
+
+function checkSign(a) {
+    return a > 0 ? "positive"
+        : a < 0 ? "negative"
+            : "zero";
+}
+
+checkSign(-12);
+console.log(checkSign(-12))
+
+
+
+
+
+
+
