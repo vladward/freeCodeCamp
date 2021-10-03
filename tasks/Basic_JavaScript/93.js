5dfbda84566d046f92280f1d04e486e522a08f2c
vladward <vladward13@gmail.com>
2021-10-03 11:31:00 +0300

    feature: Record Collection

---
+++ tasks/Basic_JavaScript/93.js
@@ -0,0 +1,49 @@
+//Record Collection
+
+// Setup
+var recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+    if (prop === "tracks" && (!(records[id]).tracks)) {
+        records[id].tracks = [];
+    }
+    if (prop === "tracks" && value !== "") {
+        records[id].tracks.push(value);
+    }
+    if (prop !== "tracks" && value !== "") {
+        records[id][prop] = value;
+    }
+    if (value === "") {
+        delete records[id][prop];
+    }
+    return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+
+
+
+
+
+
+
