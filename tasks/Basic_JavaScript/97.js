4e4de8d18cfb34a1406b81bc0325049e55e83eb5
vladward <vladward13@gmail.com>
2021-10-03 11:45:58 +0300

    feature: Count Backwards With a For Loop

---
+++ tasks/Basic_JavaScript/97.js
@@ -0,0 +1,17 @@
+//Count Backwards With a For Loop
+
+// Setup
+var myArray = [];
+for (var i=9; i>0; i-=2) {
+    myArray.push(i)
+}
+console.log(myArray)
+// Only change code below this line
+
+
+
+
+
+
+
+
