02a51d70606bd615ea3a43cdf61a792ece66b60f
vladward <vladward13@gmail.com>
2021-10-03 12:31:32 +0300

    feature: Generate Random Fractions with JavaScript

---
+++ tasks/Basic_JavaScript/102.js
@@ -0,0 +1,17 @@
+//Generate Random Fractions with JavaScript
+
+function randomFraction() {
+    var item = Math.random(2)
+    // Only change code below this line
+    return item;
+
+    // Only change code above this line
+}
+
+
+
+
+
+
+
+
