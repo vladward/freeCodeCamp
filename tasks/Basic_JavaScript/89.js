8c465fae8b3e2a76538379790c11a612f7bd163f
vladward <vladward13@gmail.com>
2021-10-03 11:14:06 +0300

    feature: Testing Objects for Properties

---
+++ tasks/Basic_JavaScript/89.js
@@ -0,0 +1,20 @@
+//Testing Objects for Properties
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if (obj.hasOwnProperty(checkProp)) {
+        var result = obj[checkProp];
+        return result;
+    } else {
+        return "Not Found";
+    }
+    // Only change code above this line
+}
+
+
+
+
+
+
+
+
