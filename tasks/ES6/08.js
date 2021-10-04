99aed7491e7f183266c7112d2884d770292e5567
vladward <vladward13@gmail.com>
2021-10-04 15:28:26 +0300

    feature: Set Default Parameters for Your Functions

---
+++ tasks/ES6/08.js
@@ -0,0 +1,7 @@
+/*
+Set Default Parameters for Your Functions
+ */
+
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
