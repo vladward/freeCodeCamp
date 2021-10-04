64e3a012176e2920bf33c347891317eefdfadd58
vladward <vladward13@gmail.com>
2021-10-04 19:02:01 +0300

    feature: Match Numbers and Letters of the Alphabet

---
+++ tasks/Regular_Expressions/10.js
@@ -0,0 +1,7 @@
+/*
+Match Numbers and Letters of the Alphabet
+ */
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
