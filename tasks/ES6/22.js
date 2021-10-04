ec30743a11ab44895eefe9e1ddd6b9807c2db256
vladward <vladward13@gmail.com>
2021-10-04 17:29:54 +0300

    feature: Create a Module Script

---
+++ tasks/ES6/22.js
@@ -0,0 +1,11 @@
+/*
+Create a Module Script
+ */
+
+<html>
+<body>
+<!-- Only change code below this line -->
+<script type="module" src="index.js"></script>
+<!-- Only change code above this line -->
+</body>
+</html>
