d466c02a91b4782ba954825d7c315bc909c6b288
vladward <vladward13@gmail.com>
2021-10-04 16:12:29 +0300

    feature: Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements

---
+++ tasks/ES6/15.js
@@ -0,0 +1,12 @@
+/*
+Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+ */
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [a, b, ...arr] = list// Change this line
+    // Only change code above this line
+    return arr;
+}
+const arr = removeFirstTwo(source);
