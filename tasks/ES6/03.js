2330e8030b3168d86a5bb8a8d5d944324fd04195
vladward <vladward13@gmail.com>
2021-10-04 15:18:34 +0300

    feature: Declare a Read-Only Variable with the const Keyword

---
+++ tasks/ES6/03.js
@@ -0,0 +1,17 @@
+/*
+Declare a Read-Only Variable with the const Keyword
+ */
+
+function printManyTimes(str) {
+
+    // Only change code below this line
+
+    const SENTENCE = str + " is cool!";
+    for (let i = 0; i < str.length; i+=2) {
+        console.log(SENTENCE);
+    }
+
+    // Only change code above this line
+
+}
+printManyTimes("freeCodeCamp");
