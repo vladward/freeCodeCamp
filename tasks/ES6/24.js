e307f2b3beded803426f46eb500e617a67f33fe3
vladward <vladward13@gmail.com>
2021-10-04 17:33:40 +0300

    feature: Reuse JavaScript Code Using import

---
+++ tasks/ES6/24.js
@@ -0,0 +1,9 @@
+/*
+Reuse JavaScript Code Using import
+ */
+
+import { lowercaseString, uppercaseString } from './string_functions.js'
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
