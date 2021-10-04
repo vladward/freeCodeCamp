ae4b80a01218650851aab7fb208b1d087eb9e0fc
vladward <vladward13@gmail.com>
2021-10-04 15:39:25 +0300

    feature: Use Destructuring Assignment to Extract Values from Objects

---
+++ tasks/ES6/11.js
@@ -0,0 +1,15 @@
+/*
+Use Destructuring Assignment to Extract Values from Objects
+ */
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// Only change code above this line
