b1c088c56eb6c856f17df5e54c1b7a94cbecacc4
vladward <vladward13@gmail.com>
2021-10-04 18:50:06 +0300

    feature: Match Anything with Wildcard Period

---
+++ tasks/Regular_Expressions/07.js
@@ -0,0 +1,7 @@
+/*
+Match Anything with Wildcard Period
+ */
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /un./; // Change this line
+let result = unRegex.test(exampleStr);
