c4030a1dc4875ddfcd0c3abf2f171a554dc97098
vladward <vladward13@gmail.com>
2021-10-05 11:17:56 +0300

    feature: Match All Letters and Numbers

---
+++ tasks/Regular_Expressions/18.js
@@ -0,0 +1,7 @@
+/*
+Match All Letters and Numbers
+ */
+
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/gi; // Change this line
+let result = quoteSample.match(alphabetRegexV2);
