adfb202c86abc9315dede7e2a6eb40caf2f90a99
vladward <vladward13@gmail.com>
2021-10-05 11:11:45 +0300

    feature: Match Beginning String Patterns

---
+++ tasks/Regular_Expressions/16.js
@@ -0,0 +1,7 @@
+/*
+Match Beginning String Patterns
+ */
+
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let result = calRegex.test(rickyAndCal);
