63d80c3b713db6f56612b21a3dff625ca3ed24a0
vladward <vladward13@gmail.com>
2021-10-05 12:09:19 +0300

    feature: Use Capture Groups to Search and Replace

---
+++ tasks/Regular_Expressions/32.js
@@ -0,0 +1,8 @@
+/*
+Use Capture Groups to Search and Replace
+ */
+
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
