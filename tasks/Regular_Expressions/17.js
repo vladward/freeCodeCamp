8297716e35a7e529b16b421ba632a67be9f4128a
vladward <vladward13@gmail.com>
2021-10-05 11:14:45 +0300

    feature: Match Ending String Patterns

---
+++ tasks/Regular_Expressions/17.js
@@ -0,0 +1,7 @@
+/*
+Match Ending String Patterns
+ */
+
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
