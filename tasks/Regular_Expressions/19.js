9a09bbc233266739d844e89d294072fbc9a87e85
vladward <vladward13@gmail.com>
2021-10-05 11:19:27 +0300

    feature: Match Everything But Letters and Numbers

---
+++ tasks/Regular_Expressions/19.js
@@ -0,0 +1,7 @@
+/*
+Match Everything But Letters and Numbers
+ */
+
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
