f5e33ff64a2b7f901cce22dc1aec9a79021d4975
vladward <vladward13@gmail.com>
2021-10-12 14:07:57 +0300

    feature: Use typeof to Check the Type of a Variable

---
+++ tasks/Debugging/03.js
@@ -0,0 +1,10 @@
+/*
+Use typeof to Check the Type of a Variable
+ */
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
