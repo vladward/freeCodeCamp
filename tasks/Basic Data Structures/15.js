ffbe8927a16cf229f16fe2c40e0200b372090bc0
vladward <vladward13@gmail.com>
2021-10-21 18:56:53 +0300

    feature: Access Property Names with Bracket Notation

---
+++ tasks/Basic Data Structures/15.js
@@ -0,0 +1,21 @@
+/*
+Access Property Names with Bracket Notation
+ */
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+    // Only change code below this line
+    let inv = foods[scannedItem]
+    return inv
+    // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
