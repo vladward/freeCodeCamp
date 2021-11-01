177a0f5b70a2526608188a45fd2b270305f500e5
vladward <vladward13@gmail.com>
2021-11-01 14:20:50 +0300

    feature: Confirm the Ending

---
+++ tasks/Basic_Algorithm_Scripting/06.js
@@ -0,0 +1,12 @@
+/*
+Confirm the Ending
+ */
+
+function confirmEnding(str, target) {
+    // "Never give up and good luck will find you."
+    // -- Falcor
+
+    return str.slice(str.length - target.length) === target;
+}
+
+confirmEnding("He has to give me a new name", "name");
