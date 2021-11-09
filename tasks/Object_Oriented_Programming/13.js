5e57bbdd6337062746a3bb046aab24bf9bcde15e
vladward <vladward13@gmail.com>
2021-11-09 19:36:31 +0300

    feat: Change the Prototype to a New Object

---
+++ tasks/Object_Oriented_Programming/13.js
@@ -0,0 +1,19 @@
+/*
+Change the Prototype to a New Object
+ */
+
+function Dog(name) {
+    this.name = name
+}
+
+Dog.prototype = {
+    // Only change code below this line
+    numLegs: 2,
+    eat: function() {
+        console.log("nom nom nom")
+    },
+    describe: function() {
+        console.log("My name is " + this.name);
+    }
+};
+
