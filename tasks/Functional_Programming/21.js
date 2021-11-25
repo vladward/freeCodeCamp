dd343e90fcbabbe8addb892843b56d42433a5481
vladward <vladward13@gmail.com>
2021-11-25 15:39:04 +0300

    feat: Apply Functional Programming to Convert Strings to URL Slugs

---
+++ tasks/Functional_Programming/21.js
@@ -0,0 +1,9 @@
+/*
+Apply Functional Programming to Convert Strings to URL Slugs
+ */
+
+// Only change code below this line
+function urlSlug(title) {
+    return title.toLowerCase().trim().split(/\s+/).join("-");
+}
+// Only change code above this line
