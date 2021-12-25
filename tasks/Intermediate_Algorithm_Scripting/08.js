469d50012ccbcaa8e12de622184b41abdb25b884
vladward <vladward13@gmail.com>
2021-12-25 19:57:42 +0300

    feat: DNA Pairing

---
+++ tasks/Intermediate_Algorithm_Scripting/08.js
@@ -0,0 +1,16 @@
+/*
+DNA Pairing
+ */
+
+function pairElement(str) {
+    var pairs = {
+        A: "T",
+        T: "A",
+        C: "G",
+        G: "C"
+    };
+    var arr = str.split("");
+    return arr.map(x => [x, pairs[x]]);
+}
+
+pairElement("GCG");
