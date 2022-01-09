d7f8df8ddbf725fc994a21db3ef45b9acd1b63fe
vladward <vladward13@gmail.com>
2022-01-09 17:43:20 +0300

    feat: Steamroller

---
+++ tasks/Intermediate_Algorithm_Scripting/16.js
@@ -0,0 +1,10 @@
+/*
+Steamroller
+*/
+
+function steamrollArray(arr) {
+    const flat = [].concat(...arr);
+    return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
